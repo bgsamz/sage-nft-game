49dedfcd6b887591e3a7d7e5b4dfacfdb0973a0f
bgsamz <bgsamz@gmail.com>
2022-03-17 00:40:15 -0700

    Select character/rendering/connect wallet.

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,128 @@
-import React from 'react';
-import twitterLogo from './assets/twitter-logo.svg';
+import { ethers } from 'ethers';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import sageGif from './assets/sage-yawn.gif';
+import twitterLogo from './assets/twitter-logo.svg';
+import SelectCharacter from './Components/SelectCharacter';
+import { CONTRACT_ADDRESS } from "./utils/constants";
+import { transformCharacterData } from "./utils/helpers";
+import sageGame from './utils/SageGame.json';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'bgsamz';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [characterNFT, setCharacterNFT] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask installed!");
+        return;
+      } else {
+        console.log("Ethereum object found.");
+
+        const accounts = await ethereum.request({method: 'eth_accounts'});
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          setCurrentAccount(account);
+        } else {
+          console.log("No authorized accounts found.");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkNetwork = async () => {
+    try {
+      let chainId = await window.ethereum.request({ method: 'eth_chainId' });
+      if (chainId !== '0x4') {
+        alert("Please connect to Rinkeby!")
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get MetaMask to continue!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Connected!", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderContent = () => {
+    if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <img
+            src={sageGif}
+            alt="Sage Yawning Gif"
+          />
+          <button className="cta-button connect-wallet-button" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        </div>
+      );
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    checkNetwork();
+  }, []);
+
+  useEffect(() => {
+    const fetchNFTMetadata = async () => {
+      console.log('Checking for Character NFT on address:', currentAccount);
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      console.log("getting contract", CONTRACT_ADDRESS);
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        sageGame.abi,
+        signer
+      );
+
+      const txn = await gameContract.checkIfUserHasNFT();
+      if (txn.name) {
+        console.log('User has Sage NFT');
+        setCharacterNFT(transformCharacterData(txn));
+      } else {
+        console.log('No Sage NFT found');
+      }
+    }
+
+    if (currentAccount) {
+      fetchNFTMetadata();
+    }
+  }, [currentAccount]);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
-          <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
-            />
-          </div>
+          <p className="header gradient-text">🐈‍⬛ Sage Tamer 🐈‍⬛</p>
+          <p className="sub-text">Team up to tame all the Sages!</p>
+          {renderContent()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -27,7 +131,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
+          >{`@${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
